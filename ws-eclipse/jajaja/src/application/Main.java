63fa442cc6a60a6b4ffbcd48a4e7a1f52b4cff60
Lucas Araújo <lucaraj1412@gmail.com>
2021-01-05 12:49:34 -0300

    quesito04.1 completo

--- ws-eclipse/jajaja/src/application/Main.java
+++ ws-eclipse/jajaja/src/application/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage){
 		try {
-		Parent root = FXMLLoader.load(getClass().getResource("/application/FXMLisso.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("application/FXMLisso.fxml"));
 		
 		Scene scene = new Scene(root,300,400);
 		scene.getStylesheets().add(getClass().getResource("appication.css").toExternalForm());
---
+++ ws-eclipse/novaTentativa/src/application/Controller.java
@@ -0,0 +1,19 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class Controller {
+	  @FXML
+	    private Label l1;
+	  @FXML
+	    private TextArea tArea;
+	  @FXML
+	    void butaoAcao(ActionEvent event) {
+		  String caracteres = tArea.getText();
+	    	l1.setText("O texto tem " + caracteres.replaceAll("\\s","").length() + " caracteres");
+
+	    }
+
+}
