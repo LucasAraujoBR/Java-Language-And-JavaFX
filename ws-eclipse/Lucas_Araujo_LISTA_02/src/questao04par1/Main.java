3dc401e3a99c0dd5aa790ea69f799e1d0a534f7e
Lucas Araújo <lucaraj1412@gmail.com>
2021-01-05 21:20:13 -0300

    Lista 2 finalizada

---
+++ ws-eclipse/Lucas_Araujo_LISTA_02/src/questao04par1/Main.java
@@ -0,0 +1,26 @@
+package questao04par1;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Pane root = FXMLLoader.load(getClass().getResource("FXMLmontar.fxml"));
+		Scene scene = new Scene(root,300,300);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		primaryStage.setTitle("Contador de caracteres APP");
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
